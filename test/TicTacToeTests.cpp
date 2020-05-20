4ae94d77e896c9d8d15462a255262dd3f6c019ef
lauedoucet <laurencedoucet@live.ca>
2020-05-20 12:44:30 -0400

    Added testing for isFull().

--- test/TicTacToeTests.cpp
+++ test/TicTacToeTests.cpp
@@ -5,14 +5,35 @@ struct TicTacToeTests
     : public::testing::Test
 {
     virtual void SetUp() override {
+        for (int i=0; i<27; i++) {
+            emptyBoard[i] = i;
+        }
 
+        for (int i=0; i<27; i++) {
+            fullBoard[i] = 'X';
+        }
     }
     
     virtual void TearDown() override {
+        for (int i=0; i<27; i++)
+        {
+            emptyBoard[i] = i;
+        }
+
+        for (int i=0; i<27; i++) {
+            fullBoard[i] = 'X';
+        }
 
     }
+
+    int emptyBoard[27];
+    int fullBoard[27]; 
 };
 
-TEST(TicTacToeTests, isFullTest) {
-    // testing stuff
+TEST_F(TicTacToeTests, isFullTest) {
+    EXPECT_FALSE(isFull(emptyBoard));
+    EXPECT_TRUE(isFull(fullBoard));
+
+    emptyBoard[4] = 'O';
+    EXPECT_FALSE(isFull(emptyBoard));
 }
